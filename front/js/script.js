2933299d07b26a48b4a4f5a2df2c32feb75c8c1c
Sébastien <inoservice59@gmail.com>
2021-10-01 23:15:13 +0200

    ..

--- front/js/script.js
+++ front/js/script.js
@@ -8,23 +8,29 @@ async function appelProduit() { // fonction asyncrone  qui permet d'appeler les 
         })
 }
 
-function afficherProduit(articles) { // création d'une fonction afin d'afficher les produits
-    const template = document.querySelector("article");
-    const clone = document.importNode(template.content, true);
+function afficherProduit(articles, premier) { // création d'une fonction afin d'afficher les produits
+    let template = document.querySelector(".items a");
+    let clone = template;
 
+    if (!premier) {
+        clone = document.importNode(template, true);
+    }
     clone.querySelector(".produit img").setAttribute("src", articles.imageUrl); // Création des clones de chaques élements afin de les appeler 
     clone.querySelector(".nom").textContent = articles.name;
     clone.querySelector(".prix").textContent = articles.price / 100 + "€";
-    clone.querySelector(".btnproduit").setAttribute("href", "produit.html?id=" + articles._id);
+    clone.querySelector(".productDescription").textContent = articles.description;
+    clone.setAttribute("href", "./product.html?id=" + articles._id);
 
-    document.querySelector("section").appendChild(clone);
+    document.querySelector(".items").appendChild(clone);
 
 }
 
 async function main() {
     let products = await appelProduit();
+    let premier = true;
     for (article of products) { //création d'une boucle for pour appeller chaque articles dans products
-        afficherProduit(article);
+        afficherProduit(article, premier);
+        premier = false;
     }
 }
 
